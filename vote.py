65fb039dc0624bbd9e2f5afb9b90545e0a47f13e
adianliusie <31857445+adianliusie@users.noreply.github.com>
2024-06-04 21:35:27 +0100

    Update vote.py

--- vote.py
+++ vote.py
@@ -14,7 +14,7 @@ def vote():
     driver = webdriver.Chrome()
 
     #driver.get('https://www.10best.com/awards/travel/best-wellness-retreat/eupepsia-bland-va/');
-    driver.get('bit.ly/458INKO')
+    driver.get('https://bit.ly/458INKO')
     time.sleep(3)
     driver.find_element_by_id("awardVoteButton").click()
     time.sleep(3)
