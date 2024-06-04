e59801fae4ef9be0f337de699c966d90be7e7e09
Adian Liusie <adianliusie@Adians-MacBook-Pro.local>
2024-06-04 21:33:41 +0100

    added bitly

--- vote.py
+++ vote.py
@@ -14,7 +14,7 @@ def vote():
     driver = webdriver.Chrome()
 
     #driver.get('https://www.10best.com/awards/travel/best-wellness-retreat/eupepsia-bland-va/');
-    driver.get('https://10best.usatoday.com/awards/travel/best-wellness-retreat-center-2024/eupepsia-wellness-resort-bland-virginia/')
+    driver.get('https://bit.ly/458INKO')
     time.sleep(3)
     driver.find_element_by_id("awardVoteButton").click()
     time.sleep(3)
