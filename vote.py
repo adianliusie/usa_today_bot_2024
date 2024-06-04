0381ddebce329a6860b2a2f0a963677400d276a7
adianliusie <31857445+adianliusie@users.noreply.github.com>
2024-06-04 21:25:34 +0100

    Update vote.py

--- vote.py
+++ vote.py
@@ -14,7 +14,7 @@ def vote():
     driver = webdriver.Chrome()
 
     #driver.get('https://www.10best.com/awards/travel/best-wellness-retreat/eupepsia-bland-va/');
-    driver.get('https://10best.usatoday.com/awards/travel/best-wellness-retreat-center-2024/eupepsia-wellness-resort-bland-virginia/')
+    driver.get('bit.ly/458INKO')
     time.sleep(3)
     driver.find_element_by_id("awardVoteButton").click()
     time.sleep(3)
@@ -22,7 +22,7 @@ def vote():
 
 def vote_script():
     count = 0
-    while count < 130:
+    while count < 250:
         try:
            vote()
         except:
