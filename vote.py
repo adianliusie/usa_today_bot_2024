d638d0b927a3311be65d96f7f22237498aa8668c
adianliusie <31857445+adianliusie@users.noreply.github.com>
2024-06-05 10:26:26 +0100

    Update vote.py

--- vote.py
+++ vote.py
@@ -12,14 +12,15 @@ import random
 
 def vote():
     driver = webdriver.Chrome()
-
     #driver.get('https://www.10best.com/awards/travel/best-wellness-retreat/eupepsia-bland-va/');
     driver.get('https://bit.ly/458INKO')
     time.sleep(3)
+    driver.find_element_by_id("onetrust-reject-all-handler").click()
+    time.sleep(1)
     driver.find_element_by_id("awardVoteButton").click()
-    time.sleep(3)
+    time.sleep(2)
     driver.quit()
-
+    
 def vote_script():
     count = 0
     while count < 250:
